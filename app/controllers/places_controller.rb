153be4fcbdacee36d37f7000ec388e22f772f1ca
Mark Folashade <ayodeji.folashade@gmail.com>
2013-08-06 20:33:54 -0500

    Added Infowindows to markers

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,16 @@
 class PlacesController < ApplicationController
   def index
     @locations = Locations.all
-    @json = @locations.to_gmaps4rails
+    @json = @locations.to_gmaps4rails do |place, marker|
+    marker.infowindow render_to_string(:partial => "/places/infowindow", :locals => { :place => place})
+    marker.title "#{place.name}"
+=begin
+    marker.json({ :population => city.population})
+    marker.picture({:picture => "http://mapicons.nicolasmollet.com/     wp-content/uploads/mapicons/shape-default/color-3875d7/shapeco     lor-color/shadow-1/border-dark/symbolstyle-contrast/symbolshad     owstyle-dark/gradient-iphone/information.png",
+                    :width => 32,
+                    :height => 32})
+=end
+    end
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @json  } #@locations
